e11dd2cea36f137a3122c64cf9b14983b0d1471d
Manuel Bärenz <programming@manuelbaerenz.de>
2024-01-03 15:15:41 +0100

    Fourmolu

--- src/Control/Monad/Trans/List.hs
+++ src/Control/Monad/Trans/List.hs
@@ -1,10 +1,11 @@
 module Control.Monad.Trans.List where
 
 -- base
-import Data.Functor.Classes
+
 import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
+import Data.Functor.Classes
 
 -- transformers
 import Control.Monad.Trans.Class
@@ -12,6 +13,7 @@ import Control.Monad.Trans.Class
 -- free
 import Control.Monad.Trans.Free.Ap
 
-newtype ListT m a = ListT { getListT :: FreeT [] m a }
-  deriving (Functor, Applicative, Monad, Alternative, MonadPlus, MonadIO, MonadTrans, Eq1, Ord1, Read1, Show1, Eq, Ord, Read, Show)
-    -- FIXME derive more type classes from mtl, exceptions? See https://hackage.haskell.org/package/free-5.1.3/docs/Control-Monad-Trans-Free-Ap.html#t:FreeT
+newtype ListT m a = ListT {getListT :: FreeT [] m a}
+    deriving (Functor, Applicative, Monad, Alternative, MonadPlus, MonadIO, MonadTrans, Eq1, Ord1, Read1, Show1, Eq, Ord, Read, Show)
+
+-- FIXME derive more type classes from mtl, exceptions? See https://hackage.haskell.org/package/free-5.1.3/docs/Control-Monad-Trans-Free-Ap.html#t:FreeT
