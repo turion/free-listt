f14ae11bcf7a0aa73e365bbbdd08d152e0c4dbe0
Manuel Bärenz <programming@manuelbaerenz.de>
2024-01-03 17:03:03 +0100

    Derive more classes and add more utilities

--- src/Control/Applicative/Trans/List.hs
+++ src/Control/Applicative/Trans/List.hs
@@ -2,6 +2,7 @@
 module Control.Applicative.Trans.List where
 
 -- base
+import Control.Applicative (Alternative)
 import Data.Functor.Compose
 
 {- | The 'Applicative' list transformer.
@@ -9,7 +10,7 @@ import Data.Functor.Compose
 This is isomorphic to the "old" @ListT@ transformer.
 It is not a monad, but a lawful 'Applicative'.
 -}
-newtype ListT f a = ListT {getListT :: f [a]}
+newtype ListT f a = ListT {runListT :: f [a]}
   deriving (Functor)
   deriving
     (Applicative, Alternative)
--- src/Control/Monad/Trans/List.hs
+++ src/Control/Monad/Trans/List.hs
@@ -10,8 +10,19 @@ import Data.Functor.Classes
 -- transformers
 import Control.Monad.Trans.Class
 
+-- mtl
+import Control.Monad.Error.Class
+import Control.Monad.RWS (MonadRWS, MonadReader, MonadState)
+import Control.Monad.Writer
+
+-- exceptions
+import Control.Monad.Catch (MonadCatch, MonadThrow)
+
 -- free
 import Control.Monad.Trans.Free
+
+-- free-listt
+import Control.Applicative.Trans.List qualified as Applicative
 
 {- | The free list monad transformer.
 
@@ -19,6 +30,44 @@ It is implemented as a rose tree (see https://en.wikipedia.org/wiki/Rose_tree)
 of computations in @m@.
 -}
 newtype ListT m a = ListT {getListT :: FreeT [] m a}
-  deriving (Functor, Applicative, Monad, Alternative, MonadPlus, MonadIO, MonadTrans, Eq1, Ord1, Read1, Show1, Eq, Ord, Read, Show)
+  deriving
+    ( Functor
+    , Applicative
+    , Monad
+    , Alternative
+    , MonadPlus
+    , MonadIO
+    , MonadFail
+    , MonadTrans
+    , Eq1
+    , Ord1
+    , Read1
+    , Show1
+    , Eq
+    , Ord
+    , Read
+    , Show
+    , MonadError e
+    , MonadState s
+    , MonadReader r
+    , MonadRWS r w s
+    , MonadWriter w
+    , MonadCatch
+    , MonadThrow
+    )
 
--- FIXME derive more type classes from mtl, exceptions? See https://hackage.haskell.org/package/free-5.1.3/docs/Control-Monad-Trans-Free-Ap.html#t:FreeT
+-- | Flatten all layers of computations to a single one.
+flatten :: (Monad m) => ListT m a -> Applicative.ListT m a
+flatten = Applicative.ListT . runListT
+
+-- | Like 'flatten', but remove the 'Applicative.ListT' wrapper.
+runListT :: (Monad m) => ListT m a -> m [a]
+runListT = iterT (fmap concat . sequence) . fmap pure . getListT
+
+-- | Construct a 'ListT' from an effectful list (a single layer).
+listT :: (Monad m) => m [a] -> ListT m a
+listT = ListT . FreeT . fmap (Free . fmap pure)
+
+-- | Construct a 'ListT' from a single effectful value.
+singleton :: (Functor m) => m a -> ListT m a
+singleton = ListT . FreeT . fmap Pure
